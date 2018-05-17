41090c5c9ee041187134a26e7408461573bd4dac
Avinashsng19 <39324845+Avinashsng19@users.noreply.github.com>
2018-05-17 20:29:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,27 @@
   <html lang="en">
     <head>
       <title>Online Blood Bank System</title>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE-Edge">
+      <link href="css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+      <nav class="navbar navbar-default navbar-fixed-top">
+      </nav>
       
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      <script src="js/jquery.js"></script>
+      <script src="js/bootstrap.min.js></script>
+    </body>
+</html>
